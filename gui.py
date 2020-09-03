ba7a28935a8c9147d573f4dd30be5e6baf09beb1
joeperpetua <qwerty.perpetua@gmail.com>
2020-09-03 18:09:03 +0200

    Added error messages, progress bar, some window settings, appJar lib, and fixed some visuals

--- gui.py
+++ gui.py
@@ -16,9 +16,15 @@ destination = "C:" + bar + "tickets" + bar
 name = ''
 path = ''
 
-
 # create a GUI variable called app
-app = gui("Debug Analyzer", "600x300")
+app = gui("Debug Analyzer", "600x600")
+app.setResizable(canResize=False)
+app.setIcon('img/debug.gif')
+app.setFg('white', override=False)
+app.setBg('#263238', override=False, tint=False)
+app.setFont(size=12, family="Verdana", underline=False, slant="roman")
+
+
 
 def isx64():
     if 'PROCESSOR_ARCHITEW6432' in os.environ:
@@ -28,7 +34,8 @@ def isx64():
 
 def clearFileInput():
     print('clear file')
-    app.clearEntry("file") 
+    app.clearEntry("file")
+    app.setMeter("progress", 0)
 
 
 def openWithVS(path, name):
@@ -52,7 +59,7 @@ def openWithSub(path, name):
     else:
         sublimeExecutable = '"C:' + bar + 'Program Files (x86)' + bar + 'Sublime Text 3' + bar + 'subl.exe"'
     
-    cmdSubl = sublimeExecutable + ' -n ' + destination + name
+    cmdSubl = sublimeExecutable + ' -a ' + destination + name
     try:
         os.system(cmdSubl)
     except Exception as err:
@@ -76,9 +83,12 @@ def openWithSub(path, name):
 
 def unZip(path, name):
     print('unzip :' + path + "\nwith name : " + name)
-    cmd7z = "7z" + ' x ' + path + ' -o' + destination + name
+    cmd7z = "7z" + ' x ' + path + ' -bsp1 -o' + destination + name
     try:
+        for x in range(0, 11):
+            app.setMeter("progress", x*random.randint(1, 10))
         os.system(cmd7z)
+        app.setMeter("progress", 100)
     except Exception as err:
         print(str(datetime.now()) + " debug analizer: Error occurred while extracting file! \n")
         exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -95,11 +105,11 @@ def btnPress(btn):
         name = app.getEntry("fileName")
         path = app.getEntry("file")
 
-    if btn == 'Open with Visual Studio':
+    if btn == 'vscode':
         check(path, name, btn)
         #openWithVS(path, name)
 
-    if btn == 'Open with Sublime Text':
+    if btn == 'sublime':
         check(path, name, btn)
         #openWithSub(path, name)  
 
@@ -108,6 +118,7 @@ def check(path, name, btn):
     # Check path possible scenarios
     if path == '':
         print('please select a file')
+        app.warningBox("No file selected", "Please select a file to analyze", parent=None)
         return 0
     elif os.path.exists(path):
         print('path is valid')
@@ -118,6 +129,7 @@ def check(path, name, btn):
         name = 'default_' + str(random.randint(0, 999999))
     elif os.path.exists(destination + name):
         print('Ticket already exists')
+        app.warningBox("Existing ticket", "Ticket already exists, please try with another custom name", parent=None)
         return 0
 
     # Check if programs are installed
@@ -177,10 +189,10 @@ def check(path, name, btn):
         
     unZip(path, name)
 
-    if btn == 'Open with Visual Studio':
+    if btn == 'vscode':
         openWithVS(path, name)
 
-    if btn == 'Open with Sublime Text':
+    if btn == 'sublime':
         openWithSub(path, name)  
     
   
@@ -190,7 +202,7 @@ if app:
     #app.setSticky("news")
     #app.setExpand("both")
     app.setStretch("both")
-    app.setFont(14)
+
 
     app.startFrame("header", row=0, column=0)
     app.addLabel("l1", " ", row=0, column=0, colspan=1, rowspan=1)
@@ -199,25 +211,55 @@ if app:
     app.stopFrame()
 
     app.setPadding([60,10])
-    app.startFrame("body", row=1, column=0, colspan=4, rowspan=2)
+    app.startFrame("body", row=1, column=0, colspan=4, rowspan=1)
     app.setPadding([0,0])
 
     # File name input
-    app.addLabel("name label", "Custom name\nfor the file", row=0, column=0, colspan=2)
+    app.setSticky("ew")
+    app.addLabel("name label", "Ticket n.", row=0, column=0, colspan=2)
     app.addEntry("fileName", row=0, column=3)
+    app.setEntryRelief("fileName", "groove")
+    app.setEntryTooltip("fileName", "If no name is provided a random name will be assigned")
     # File input
     app.addLabel("file label", "Select debug file", row=1, column=0, colspan=2)
     app.setPadding([5,5])
     app.addFileEntry("file", row=1, column=3, colspan=1)
     app.addButton("X", btnPress, row=1, column=4)
+    app.setButtonBg("X", "#e5806c")
+    app.setButtonRelief("X", "flat")
     app.setPadding([0,0])
     app.stopFrame()
 
 
-    app.startFrame("footer", row=3, column=0)
-    app.addButtons(["Open with Visual Studio", "Open with Sublime Text"], btnPress,row=0, column=0,)
-    app.setPadding([60,40])
-    app.addImage("logo", "logo.gif", row=1, column=0, colspan=2, rowspan=1)
+    app.startFrame("buttons", row=3, column=0)
+    app.addLabel("Choose", "Open with:", row=0, column=0)
+    app.addButtons(["vscode", "sublime"], btnPress,row=1, column=0,)
+    app.setButtonImage("vscode", "img/vscode.gif")
+    app.setButtonBg("vscode", "#192227")
+    app.setButtonRelief("vscode", "flat")
+
+    app.setButtonImage("sublime", "img/sublime.gif")
+    app.setButtonBg("sublime", "#192227")
+    app.setButtonRelief("sublime", "flat")
+    # app.setPadding([60,100])
+    app.addMeter("progress", row=2, column=0)
+    app.setMeterFill("progress", "#192227")
+    app.setPadding([0,0])
+    app.stopFrame()
+
+    app.startFrame("footer", row=4, column=0)
+    app.setSticky("w")
+    app.addButton("Help", btnPress, row=0, column=0, colspan=1)
+    app.setButtonBg("Help", "#4a4e50")
+    app.setButtonFg("Help", "#ffffff")
+    app.setButtonRelief("Help", "flat")
+    app.setSticky("ew")
+    app.addImage("logo", "img/logo.gif", row=0, column=1, colspan=2, rowspan=1)
+    app.setSticky("e")
+    app.addButton("Report Bug", btnPress, row=0, column=3, colspan=1)
+    app.setButtonBg("Report Bug", "#4a4e50")
+    app.setButtonFg("Report Bug", "#ffffff")
+    app.setButtonRelief("Report Bug", "flat")
     app.stopFrame()
 
 
